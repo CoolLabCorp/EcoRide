ca4faec08a8f725e01002724cc47ea6c95494c10
Bonei <boneiedifi@gmail.com>
2015-12-18 02:15:38 -0300

    added at login page

--- app/client/EcoRide/www/js/app.js
+++ app/client/EcoRide/www/js/app.js
@@ -1,4 +1,4 @@
-// Ionic Starter App
+  // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -77,9 +77,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+
+.state('tab.login', {
+    url: '/login',
+    views: {
+      'login': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+
+
+
+
+
+  ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/login');
 
 });
