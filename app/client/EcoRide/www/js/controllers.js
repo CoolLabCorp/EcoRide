c67dd9a8586486fd16db4f637492eff58e6d1835
ycalistrato <yuri.calistrato@gmail.com>
2016-02-18 22:50:26 -0300

    Fix 2

--- app/client/EcoRide/www/js/controllers.js
+++ app/client/EcoRide/www/js/controllers.js
@@ -1,32 +1,34 @@
-angular.module('starter.controllers', [])
+angular.module('app.controllers', [])
+  
+.controller('connectItCtrl', function($scope) {
 
-.controller('StatusUserCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
+   
+.controller('chatCtrl', function($scope) {
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
+   
+.controller('inboxCtrl', function($scope) {
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 })
+   
+.controller('needARideCtrl', function($scope) {
 
-.controller('LoginCtrl', function($scope) {})
+})
+      
+.controller('settingsCtrl', function($scope) {
 
-;
+})
+   
+.controller('offerARideCtrl', function($scope) {
+
+})
+   
+.controller('overviewCtrl', function($scope) {
+
+})
+   
+.controller('routineCtrl', function($scope) {
+
+})
+ 
