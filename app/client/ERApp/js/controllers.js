0adf3040bdd844c5a22550cde376f9172557b97a
ycalistrato <yuri.calistrato@gmail.com>
2016-02-18 22:46:24 -0300

    Fix

---
+++ app/client/ERApp/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('connectItCtrl', function($scope) {
+
+})
+   
+.controller('chatCtrl', function($scope) {
+
+})
+   
+.controller('inboxCtrl', function($scope) {
+
+})
+   
+.controller('needARideCtrl', function($scope) {
+
+})
+      
+.controller('settingsCtrl', function($scope) {
+
+})
+   
+.controller('offerARideCtrl', function($scope) {
+
+})
+   
+.controller('overviewCtrl', function($scope) {
+
+})
+   
+.controller('routineCtrl', function($scope) {
+
+})
+ 
